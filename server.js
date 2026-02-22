ea5558a31cd0f2f1f8243c6487d0711a8c054de0
Rebeca Murcia <131334317+RebecaMurcia@users.noreply.github.com>
2026-02-21 20:31:47 -0800

    Authentication/login

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const session = require('express-session');
+const passport = require('passport');
+const GitHubStrategy = require('passport-github2').Strategy;
 
 const app = express();
 
@@ -37,10 +40,66 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // Middleware to parse JSON
 app.use(express.json());
 
+// Session Configuration
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
+
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// GitHub Strategy Configuration
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 // Import Routes
 const itemsRouter = require('./routes/items');
 const errorHandler = require('./middleware/error');
+
+// Authentication Route - Login Route
+app.get('/login', passport.authenticate('github', { scope: [ 'user:email' ] }));
+
+// Authentication Route - Callback Route
+app.get('/github/callback', 
+  passport.authenticate('github', { failureRedirect: '/api-docs', session: false }),
+  (req, res) => {
+    req.session.user = req.user;
+    res.send('Logged in successfully! Go to <a href="/api-docs">Swagger Docs</a> to test routes.');
+  }
+);
+
+// Authentication Route - Logout Route
+app.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) { return next(err); }
+        req.session.destroy();
+        res.redirect('/api-docs');
+    });
+});
+
+// Authentication Route - Status Route
+app.get('/auth/status', (req, res) => {
+    res.json(req.session.user !== undefined ? { loggedIn: true, user: req.session.user } : { loggedIn: false });
+});
+
 
 // Use Routes
 app.use('/api/items', itemsRouter); 
